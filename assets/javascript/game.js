e897f9eba37b6b4ddbb2ae692b10f15ed13821a5
Mike Troutman <mktrout09@gmail.com>
2017-08-11 22:43:50 -0400

    psychic_game

---
+++ assets/javascript/game.js
@@ -0,0 +1,57 @@
+
+
+var wins = 0;
+
+var losses = 0;
+
+var guessRem = 10;
+
+var compChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+document.onkeyup = function(event) {
+
+	var userGuess = event.key;
+
+	var compGuess = compChoices[Math.floor(Math.random() * compChoices.length)];
+
+	
+  function newGame() {
+       guessRem = 10;
+       userGuess = [];
+       compGuess = compChoices[Math.floor(Math.random() * compChoices.length)];
+    
+	}
+
+		if (userGuess === compGuess) {
+		
+		wins++;
+		}
+
+		else if (guessRem === 0) {
+		
+		losses++;
+		}
+
+
+
+		else {
+		
+		guessRem--
+		}
+
+
+
+	
+
+	var html =
+
+		"<p>wins: " + wins + "</p>" +
+
+		"<p>losses: " + losses + "</p>" +
+
+		"<p>Guesses Remaining: " + guessRem + "</p>" +
+
+		"<p>Your Guess: " + userGuess + "</p>";
+
+	document.querySelector("#game").innerHTML = html;
+}
